2a0570a8aca1f201da178096faa3e5394b1e43e4
Braulio Diez Botella <braulio.diez@gmail.com>
2021-04-25 13:07:48 +0200

    something working

---
+++ 02-basico-pie/src/index.ts
@@ -0,0 +1,62 @@
+import * as d3 from "d3";
+
+interface Ventas {
+  mes: string;
+  ventas: number;
+}
+
+const ventas: Ventas[] = [
+  { mes: "Enero", ventas: 5000 },
+  { mes: "Febrero", ventas: 2500 },
+  { mes: "Marzo", ventas: 3000 },
+];
+
+const nombresMeses = ventas.map((venta) => venta.mes);
+
+const color = d3
+  .scaleOrdinal<string, string>()
+  .domain(nombresMeses)
+  .range(["#006A7B", "#008D54", "#659B91", "#00BC9F", "#006B5F"]);
+
+const svgDimensions = { width: 400, height: 400 };
+const margin = { left: 5, right: 5, top: 10, bottom: 10 };
+const chartDimensions = {
+  width: svgDimensions.width - margin.left - margin.right,
+  height: svgDimensions.height - margin.bottom - margin.top,
+};
+
+const svg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", svgDimensions.width)
+  .attr("height", svgDimensions.height)
+  .attr("style", "background-color: #FBFAF0");
+
+const radio = Math.min(chartDimensions.width, chartDimensions.height) / 2;
+
+var grupoGrafica = svg
+  .append("g")
+  .attr("width", chartDimensions.width)
+  .attr("height", chartDimensions.height)
+  .attr(
+    "transform",
+    `translate({$margin.left + (chartDimensions.height / 2), {$margin.top + chartDimensiones.width /2}})`
+  );
+
+const pieLayout = d3.pie<Ventas>().value(function (d) {
+  return d.ventas;
+});
+
+var datosArcos = pieLayout(ventas);
+
+const constructorDePath = d3.arc().innerRadius(0).outerRadius(radio);
+
+grupoGrafica
+  .selectAll("slice")
+  .data(datosArcos)
+  .enter()
+  .append("path")
+  .attr("d", <any>constructorDePath)
+  .attr("fill", function (d) {
+    return color(d.data.mes);
+  });
